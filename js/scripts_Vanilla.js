2047abf4a402b55e84392d1378e2d5e1ab8efb96
doridoro <dorothea.reher@gmail.com>
2023-05-13 15:59:21 +0200

    use fetch API to erase error

--- js/scripts_Vanilla.js
+++ js/scripts_Vanilla.js
@@ -1,19 +1,18 @@
-
+var home_url = "http://localhost:8000/api/v1/titles/"
 var bestMovie = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
 
-// create a reusable function
-function getResponse(url, callback) {
-    var r = new XMLHttpRequest();
-    r.open("GET", url , true);
-    r.onreadystatechange = function () {
-      if (r.readyState != 4 || r.status != 200) {
-        var response = JSON.parse(r.responseText);
-        callback(response);
-      }
-    };
-    r.send();
+async function logJSONData(url) {
+  try {
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    console.log(jsonData);
+  } catch (error) {
+    console.error(error);
+  }
 }
+logJSONData(home_url)
 
+/*
 getResponse(bestMovie, function(response) {
     var imageUrl = response.results[0].image_url;
     var imgElement = document.createElement("img");
@@ -26,3 +25,4 @@ getResponse(bestMovie, function(response) {
     titleElement.appendChild(textTitle);
     document.getElementById("the-best").appendChild(titleElement);
 });
+*/
