166d4a481a6e380560e03c0399649d4ef68747e8
doridoro <dorothea.reher@gmail.com>
2023-05-26 19:25:38 +0200

    create carouselMovies and displayModal function

--- js/scripts_Vanilla.js
+++ js/scripts_Vanilla.js
@@ -1,11 +1,11 @@
 const homeUrl = "http://localhost:8000/api/v1/titles/"
-const categories = ["best-movie", "romance", "drama", "history"];
+const categories = ["best-movies", "romance", "drama", "history"];
 
 // generate the urls:
 async function createUrls(category) {
   let url;
 
-  if (category === "best-movie") {
+  if (category === "best-movies") {
     url = `${homeUrl}?sort_by=-imdb_score`
   } else {
     url = `${homeUrl}?genre=${category}`
@@ -62,68 +62,44 @@ async function displayImgUrl(data) {
   data.pop()
 
   try {
-    if (category === "best-movie") {
-      console.log('the best movie');
-    } else {
-      for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j < 5; j++) {
-          console.log('img', data[i][j].image_url);
-          
-        }
-        
-      }
-      /*
-      let imgContent = 
-          `
-          <div>
-              <img src="${data[i][j].image_url}" alt="${[i][j].title}">
-          </div>
-          `
-          */
+    if (category === "best-movies") {
+      const theBestMovieId = data[0][0].id;
 
-      console.log(data[0][0].image_url);
+      await theBestMovie(theBestMovieId);
+
+      await carouselMovies(data, category);
+      
+    } else {
+      await carouselMovies(data, category);
     }
-    console.log('here', data);
 
   } catch(error) {
     console.log(error);
   }
 }
 
+// two functions to display the movies and the details:
+// best-movie: 1) the best one plus details
+// best-movie: 2) 7 movies plus details
 
-async function main() {
-  const allData = await getAllData(categories[1]);
-  await displayImgUrl(allData);
-}
-
-
-main();
-
-/*
-async function getBestMovie(url) {
+// create the-best-movie section:
+async function theBestMovie(id) {
   try {
-    const response = await fetch(url);
-    const jsonData = await response.json();
+    const theBestMovieUrl = `${homeUrl}${id}`
 
-    const bestMovieId = jsonData.results[0].id;
-    const bestMovieUrl = `${homeUrl}${bestMovieId}`
-
-    const movieDetailsResponse = await fetch(bestMovieUrl);
+    const movieDetailsResponse = await fetch(theBestMovieUrl);
     const movieDetails = await movieDetailsResponse.json();
 
     const content = `
-      <div id="best-movie">
-        <h2> The Best Movie </h2>
-        <div class="container">
-          <img src=${movieDetails.image_url} alt=${movieDetails.title}>
-          <div class="movie-content">
-            <h3> ${movieDetails.title} (${movieDetails.imdb_score}/10)</h3>
-            <p> ${movieDetails.description}</p>
-          </div>
+      <div class="container">
+        <img src=${movieDetails.image_url} alt=${movieDetails.title}>
+        <div class="movie-content">
+          <h3> ${movieDetails.title} (${movieDetails.imdb_score}/10)</h3>
+          <p> ${movieDetails.description}</p>
         </div>
-        <div id="button">
-          <button id="open-modal" class="open-button button"> Show Details </button>
-        </div>
+      </div>
+      <div id="button">
+        <button id="open-modal" class="open-button button"> Show Details </button>
       </div>
 
       <div id="modal" class="modal border">
@@ -177,17 +153,17 @@ async function getBestMovie(url) {
     section.innerHTML = content;
 
     const openModal = document.getElementById("open-modal");
-    const modal = document.getElementById("modal");
+    const modalElement = document.getElementById("modal");
     const closeModal = document.getElementById("close-modal");
 
 
     openModal.addEventListener("click", function (event) {
       event.preventDefault();
-      modal.classList.add("show");
+      modalElement.classList.add("show");
     });
 
     closeModal.addEventListener("click", function () {
-      modal.classList.remove("show");
+      modalElement.classList.remove("show");
     });
 
   } catch (error) {
@@ -195,57 +171,126 @@ async function getBestMovie(url) {
   }
 }
 
-async function carousel(url1) {
-  url2 = `${url1}&page=2`
-
+// create carousel:
+async function carouselMovies(data, category) {
   try {
-    const response1 = await fetch(url1);
-    const jsonData1 = await response1.json();
-
-    const response2 = await fetch(url2);
-    const jsonData2 = await response2.json();
-
-    const carouselMovies1 = jsonData1.results;
-    const carouselMovies2 = jsonData2.results;
-
-    console.log(jsonData2)
-
     let carouselImg = `
-      <div>
-        <h2> Romance Movies </h2>
-        <div class="btn-right">
-          <button class="button"> Right </button>
-        </div>
-        <div class="container margin-left">
+      <div class="btn-right">
+        <button class="button"> Right </button>
+      </div>
+      <div class="container margin-left">
     `;
 
-    for (let i = 0; i < 5; i++) {
-      carouselImg += `
-          <div>
-            <img src="${results[i].image_url}" alt="${[i].title}">
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < 5; j++) {
+
+        carouselImg += `
+          <div id="${data[i][j].id}">
+            <img src="${data[i][j].image_url}" alt="${data[i][j].title}">
           </div>
-      `;
+        `;
+      }
     }
 
-    let button_left = `
-        </div>
-        <div class="btn-left">
-          <button class="button"> Left </button>
-        </div>
+    carouselImg += `
+      </div>
+      <div class="btn-left">
+        <button class="button"> Left </button>
       </div>
     `;
-    
-    carouselImg += button_left;
 
-    let section = document.getElementById("romance");
+    let section = document.getElementById(`${category}`);
     section.innerHTML = carouselImg;
 
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < 5; j++) {
+        const openModal = document.getElementById(`${data[i][j].id}`);
+
+        openModal.addEventListener("click", function (event) {
+        event.preventDefault();
+        displayModal(`${data[i][j].url}`, `${category}`);
+        });
+      }
+    }
   } catch (error) {
     console.error(error);
   }
+}
+
+async function displayModal(url, category) {
+  const movieDetailsResponse = await fetch(url);
+  const movieDetails = await movieDetailsResponse.json();
+
+  modalContent = `
+    <div id="modal" class="modal border">
+      <h2> title </h2>
+        <div class="container">
+          <img src=${movieDetails.image_url} alt=${movieDetails.title}>
+          <div class="movie-content">
+            <h3> ${movieDetails.title} (${movieDetails.imdb_score}/10) </h3>
+            <table>
+              <tr>
+                <th> Genre: </th>
+                <th> Country: </th>
+                <th> Published Date: </th>
+                <th> Vote: </th>
+                <th> Duration: </th>
+                <th> Budget: </th>
+              </tr>
+              <tr>
+                <td> ${movieDetails.genres} </td>
+                <td> ${movieDetails.countries} </td>
+                <td> ${movieDetails.date_published} </td>
+                <td> ${movieDetails.avg_vote} </td>
+                <td> ${movieDetails.duration} </td>
+                <td> ${movieDetails.budget} </td>
+              </tr>
+            </table>
+
+            <table id="second-table">
+              <tr>
+                <th> Director: </th>
+                <td> ${movieDetails.directors} </td>
+              </tr>
+              <tr>
+                <th> Actors: </th>
+                <td> ${movieDetails.actors} </td>
+              </tr>
+              <tr>
+                <th> Description: </th>
+                <td> ${movieDetails.long_description} </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      <div>
+        <button id="close-modal" class="close-button button"> Close </button>
+      </div>
+    </div>
+  `;
+
+  let modalElement = document.getElementById("modal");
+  modalElement.innerHTML = modalContent;
+  modalElement.classList.add("show");
+
+  const closeModal = document.getElementById("close-modal");
+
+  closeModal.addEventListener("click", function () {
+    modalElement.classList.remove("show");
+  });
 
 }
 
-carousel(romance)
-getBestMovie(bestMovie)
+
+async function main() {
+  const allData = await getAllData(categories[1]);
+  await displayImgUrl(allData);
+}
+
+
+main();
+
+
+/*
+  categoryTitle = ["The Best Movie", "Best Rated Movies", "Romance Movies", "Dramas", "History Movies"]       
 */
